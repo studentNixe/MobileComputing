74329dca6c4621a7c4fc44e452906da8ccd42269
shakurakazuki <shakura.kazuki@gmail.com>
2017-05-07 13:08:44 +0200

    new Activity objectHandlerActivity.java

---
+++ app/src/main/java/com/example/baby/firstgame/ObjectHandlerActivity.java
@@ -0,0 +1,17 @@
+package com.example.baby.firstgame;
+
+import android.app.Activity;
+import android.view.View;
+
+/**
+ * Created by Denise on 07.05.2017.
+ */
+
+public class ObjectHandlerActivity extends Activity implements View.OnClickListener{
+
+    @Override
+    public void onClick(View v) {
+
+
+    }
+}
