fb98a30ff8fef376ba6c0e1bb0baa983ebbcdd69
pawanbasnet <pa1basnet97@gmail.com>
2017-05-30 10:02:39 +0200

    "work on swiping and name creature activity"

--- app/src/main/java/com/example/baby/firstgame/ChooseCreatureActivity.java
+++ app/src/main/java/com/example/baby/firstgame/ChooseCreatureActivity.java
@@ -4,6 +4,9 @@ package com.example.baby.firstgame;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageSwitcher;
@@ -17,21 +20,28 @@ import com.example.baby.firstgame.data.CreatureHandler;
  */
 
 public class ChooseCreatureActivity extends Activity
+        implements GestureDetector.OnGestureListener
 {
+    private Button btnNext, btnPre, btnSelect;
     private ImageSwitcher creSwitcher;
+    private GestureDetectorCompat gestureDetector;
+
+    private int swipeMinDistance = 130;
+    private int swipeVelocity = 200;
 
     Integer[] creatures = {R.drawable.denise1,
             R.drawable.pawan1, R.drawable.nicole2};
     int iterate = 0;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choosecreature);
-
-        Button btnNext = (Button) findViewById(R.id.next);
-        Button btnPre = (Button) findViewById(R.id.previous);
-        Button btnSelect = (Button) findViewById(R.id.select);
+        btnNext = (Button) findViewById(R.id.next);
+        btnPre = (Button) findViewById(R.id.previous);
+        btnSelect = (Button) findViewById(R.id.select);
+        this.gestureDetector = new GestureDetectorCompat(this, this);
 
         //------------- will be deleted ----------
         CreatureHandler handler = new CreatureHandler(this);
@@ -44,10 +54,6 @@ public class ChooseCreatureActivity extends Activity
             public View makeView() {
                 ImageView imageView = new ImageView(getApplicationContext());
                 imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                //     imageView.setLayoutParams(
-//                        new ImageSwitcher.LayoutParams(
-//                                ViewGroup.LayoutParams.MATCH_PARENT,
-//                                ViewGroup.LayoutParams.MATCH_PARENT));
                 return imageView;
             }
         });
@@ -78,5 +84,53 @@ public class ChooseCreatureActivity extends Activity
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        if(e1.getX() - e2.getX() > swipeMinDistance && Math.abs(velocityX) > swipeVelocity)
+        {
+            iterate = (iterate+1) % creatures.length;
+            creSwitcher.setImageResource(creatures[iterate]);
+            return true;
+        }
+        else if (e2.getX() - e1.getX() > swipeMinDistance && Math.abs(velocityX) > swipeVelocity)
+        {
+            iterate = (iterate+1) % creatures.length;
+            creSwitcher.setImageResource(creatures[iterate]);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 }
--- app/src/main/java/com/example/baby/firstgame/NameCreatureActivity.java
+++ app/src/main/java/com/example/baby/firstgame/NameCreatureActivity.java
@@ -1,11 +1,17 @@
 package com.example.baby.firstgame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.ViewSwitcher;
+
+import com.example.baby.firstgame.data.CreatureObject;
 
 /**
  * Created by Pawan on 5/12/2017.
@@ -16,6 +22,10 @@ public class NameCreatureActivity extends Activity
     private ImageSwitcher eggSwitcher;
     Integer[] eggs = {R.drawable.denis_egg,
             R.drawable.pawan_egg, R.drawable.nicole_eggs};
+    private String[] speciesList = {"denise","pawan","nicole"};
+
+    private Button btnenter = (Button) findViewById(R.id.Enter);
+    EditText edtname = (EditText) findViewById(R.id.nameEntered);
 
     @Override
     public void onCreate(Bundle savedInstaceState){
@@ -33,6 +43,19 @@ public class NameCreatureActivity extends Activity
             }
         });
         eggSwitcher.setImageResource(eggs[creatureEgg]);
+        String species = speciesList[creatureEgg];
 
+        //creates creature here with the give name and species
+      //  CreatureObject creature = new CreatureObject(edtname.getText().toString(), species);
+
+        btnenter.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getApplicationContext(), MonsterHomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
